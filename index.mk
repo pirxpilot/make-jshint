f17e7f679d3695c471a01fb36f57aa068b7a61e3
Damian Krzeminski <pirxpilot@code42day.com>
2017-10-21 21:20:36 -0600

    remove jshint dependency
    projects should install jshint directly

--- index.mk
+++ index.mk
@@ -1,9 +1,7 @@
-DIR ?= $(dir $(lastword $(MAKEFILE_LIST)))
-
 SRC ?= *.js lib test
 LINT_SRC ?= $(SRC)
 
-LINT := $(DIR)node_modules/.bin/jshint
+LINT := ./node_modules/.bin/jshint
 
 lint:
 	$(LINT) $(LINT_SRC)
